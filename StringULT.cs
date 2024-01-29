71811fd1ed2286cd71a32ec238d6492c9f1bca3a
Zoe-sample-student <144137451+Zoe-sample-student@users.noreply.github.com>
2024-01-29 14:14:35 +0000

    added more stuff, used a formater to make it look better

--- StringULT.cs
+++ StringULT.cs
@@ -1,35 +1,38 @@
 ﻿using System;
-{
+{}
 
-
-
-}
-namespace UtilityPackage
-{
-
-    public static class StringUtils
-    {
-        public static string UpperCaseFormatter(String s)
-        {
-            return s.ToUpper();
-        }
-        public static string LowerCaseFormatter(String s)
-        {
-            return s.ToLower();
-        }
-        public static int length(string s)
-        {
-            return s.Length;
-        }
-        public static string Reverse(string s)
-        {
-            return s.Length;
-        }
-        public static string Reverse(string s)
-        { 
-            char[] charArray = s.ToCharArray
-        }
+namespace MyUtilityPackage {
+  public static class StringUtils {
+    // Method to convert a string to uppercase
+    public static string ToUpper(string s) {
+      return s.ToUpper();
     }
 
+    // Method to convert a string to lowercase
+    public static string ToLower(string s) {
+      return s.ToLower();
+    }
 
-}
+    // Method to get the length of a string
+    public static int Length(string s) {
+      return s.Length;
+    }
+
+    // Method to reverse a string
+    public static string Reverse(string s) {
+      char[] charArray = s.ToCharArray();
+      Array.Reverse(charArray);
+      return new string(charArray);
+    }
+
+    // Method to count vowels in a string
+    public static int CountVowels(string s) {
+      int count = 0;
+      foreach(char c in s) {
+        if ("AEIOUaeiou".Contains(c)) {
+          count++;
+        }
+      }
+      return count;
+    }
+  }
