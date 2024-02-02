1af83f79e2c42b6d8ae5948f7fe85037d7691768
Zoe-sample-student <144137451+Zoe-sample-student@users.noreply.github.com>
2024-02-02 14:42:48 +0000

    Making comments and made lower case method (Thanks Katie)

--- StringULT.cs
+++ StringULT.cs
@@ -1,11 +1,26 @@
-﻿public static class StringUtils
-{int i
-    public static string ToUpper(string s)
+﻿using System;
+class Program{
+public static class StringUtils
+{
+    public static string ToUpper(string s) //Lower Case to upper case (string  ----> STRING)
     {
-        string Outcome ="";
+        string outcome = "";
 
-        for( int i=0; i< s.Length; i++);
+        for (int i = 0; i < s.Length; i++)
+        {
+            
+            if (s[i]>= 'a' && s[i] <= 'z')
+            {
+                
+                outcome += (char)(s[i] - 32);
+            }
+            else
+            {
+                outcome += s[i]; 
+            }
+        }
+
+        return outcome;
     }
-    
-   
+}
 }
